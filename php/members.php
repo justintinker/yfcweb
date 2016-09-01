bed83d571803963b0de5fb7725c2a7c47c687e29
justintinker <justin.tinker1@gmail.com>
2016-08-31 20:59:55 -0400

    ^

--- php/members.php
+++ php/members.php
@@ -1,6 +1,7 @@
 <?php
 $passphrase = "citadel";
 $drivefolder = "https://drive.google.com/folderview?id=0B6-A0_C25Xw7SU9MaUhpMU14c2s&usp=sharing";
+echo "<h1>hi</h1>";
 if(strcmp($_POST["passphrase"], $passphrase) === 0) {
   echo '<script type="text/javascript">
            window.location = "' . $drivefolder .'"
