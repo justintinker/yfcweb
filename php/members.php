1541c00cf7b90975c65a38788de74b66ad2b3b07
justintinker <justin.tinker1@gmail.com>
2016-08-31 21:07:27 -0400

    ^

--- php/members.php
+++ php/members.php
@@ -2,6 +2,7 @@
 $passphrase = "citadel";
 $drivefolder = "https://drive.google.com/folderview?id=0B6-A0_C25Xw7SU9MaUhpMU14c2s&usp=sharing";
 echo "<h1>enter</h1>";
+echo "<h1>".$_POST["passphrase"]."</h1>";
 echo "<h1>".$passphrase."</h1>";
 if(strcmp($_POST["passphrase"], $passphrase) === 0) {
   echo "<h1>match</h1>";
